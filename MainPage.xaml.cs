d9b103eb9fef860d2f4f8d0242cd34f1c4f47680
jeremylindsayni <jeremy.lindsay@outlook.com>
2016-07-03 19:00:50 +0100

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Magellanic.Speech.Recognition;
+using System.Diagnostics;
+using Windows.Media.SpeechRecognition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SpeechRecognitionExample
+{
+    public sealed partial class MainPage : Page
+    {
+        private const string grammarFile = "grammar.xml";
+
+        private SpeechRecognitionManager recognitionManager;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Loaded += MainPage_Loaded;
+
+            Unloaded += MainPage_Unloaded;
+        }
+
+        private async void MainPage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            await recognitionManager.Dispose();
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            // initialise the speech recognition manager
+            recognitionManager = new SpeechRecognitionManager(grammarFile);
+
+            // register the event for when speech is detected
+            recognitionManager
+                .SpeechRecognizer
+                .ContinuousRecognitionSession
+                .ResultGenerated += RecognizerResultGenerated;
+
+            // compile the grammar file
+            await recognitionManager.CompileGrammar();
+        }
+
+        private void RecognizerResultGenerated(
+            SpeechContinuousRecognitionSession session,
+            SpeechContinuousRecognitionResultGeneratedEventArgs args)
+        {
+            // only act if the speech is recognised with high confidence
+            if (!args.Result.IsRecognisedWithHighConfidence())
+            {
+                return;
+            }
+
+            // interpret key individual parts of the grammar specification
+            string command = args.Result.SemanticInterpretation.GetInterpretation("command");
+            string direction = args.Result.SemanticInterpretation.GetInterpretation("direction");
+
+            // write to debug
+            Debug.WriteLine($"Command: {command}, Direction: {direction}");
+        }
+    }
+}
